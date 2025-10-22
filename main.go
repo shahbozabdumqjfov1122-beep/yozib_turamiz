083ea9b78b0b6b0c1cf4123468c6675e67e36911
shahboz <shahbozabdumqjfov1122@gmail.com>
2025-10-22 16:26:12 +0500

    fdrfgrg

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -34,23 +33,11 @@ func main() {
 				msg.ReplyToMessageID = update.Message.MessageID
 
 				// Xabarni yuborish
-				sentMsg, err := bot.Send(msg)
+				_, err := bot.Send(msg)
 				if err != nil {
 					log.Println("Send error:", err)
-					continue
 				}
-
-				// 5 soniya keyin faqat botning o'z xabarini o'chirish
-				go func(chatID int64, messageID int) {
-					time.Sleep(5 * time.Second)
-					delMsg := tgbotapi.DeleteMessageConfig{
-						ChatID:    chatID,
-						MessageID: messageID,
-					}
-					if _, err := bot.Request(delMsg); err != nil {
-						log.Println("Delete error:", err)
-					}
-				}(chatID, sentMsg.MessageID)
+				// Hech qachon o'chirmaydi
 			}
 		}
 	}
